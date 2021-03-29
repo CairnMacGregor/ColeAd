da53deb57b50662404a8f6fbdbba60517536ce8d
CairnMacGregor <60977182+CairnMacGregor@users.noreply.github.com>
2021-03-29 17:53:56 +0100

    Fix

--- JS/App.js
+++ JS/App.js
@@ -2,7 +2,6 @@ const toggleButton = document.getElementById("toggler");
 const body = document.getElementsByTagName("BODY")[0];
 const stopScroll = () => {
     if(toggleButton.checked == true){
-        console.log("True");
     body.classList.add('stopScroll');
     } else {
         body.classList.remove('stopScroll');
